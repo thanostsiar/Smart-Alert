15c7a95f335427cb0622bc14d56d73b8c7b1bda5
jasonbarberis <59922675+jasonbarberis@users.noreply.github.com>
2023-02-10 18:15:00 +0200

    User can now navigate to the Sign Up page

---
+++ SmartAlert/app/src/main/java/com/unipi/tsiaras/smartalert/SignIn.java
@@ -0,0 +1,30 @@
+package com.unipi.tsiaras.smartalert;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignIn extends AppCompatActivity {
+
+    Button btn_sign_up;
+    Button btn_sign_in;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_in);
+        btn_sign_up = findViewById(R.id.button_signup);
+        btn_sign_in = findViewById(R.id.button_signin);
+
+        btn_sign_up.setOnClickListener(v -> {
+            OpenSignUp();
+        });
+
+
+    }
+    public void OpenSignUp(){
+        Intent intent = new Intent(getApplicationContext(), SignUp.class);
+        startActivity(intent);
+    }
+}
