330db85b922ccfd221d8a6857cb841a0ddc517c9
thanostsiar <thanososfp.tt@gmail.com>
2023-02-11 20:32:13 +0200

    Finished Sign In

--- SmartAlert/app/src/main/java/com/unipi/tsiaras/smartalert/SignIn.java
+++ SmartAlert/app/src/main/java/com/unipi/tsiaras/smartalert/SignIn.java
@@ -2,19 +2,31 @@ package com.unipi.tsiaras.smartalert;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignIn extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    EditText email, pass;
     Button btn_sign_up;
     Button btn_sign_in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sing_in);
+        setContentView(R.layout.activity_sign_in);
+        email = findViewById(R.id.et_email);
+        pass = findViewById(R.id.et_password);
         btn_sign_up = findViewById(R.id.button_signup);
         btn_sign_in = findViewById(R.id.button_signin);
+
+        mAuth = FirebaseAuth.getInstance();
         btn_sign_up.setOnClickListener(v -> {
             OpenSignUp();
         });
@@ -24,5 +36,27 @@ public class SignIn extends AppCompatActivity {
     public void OpenSignUp(){
         Intent intent = new Intent(getApplicationContext(), SignUp.class);
         startActivity(intent);
+    }
+
+    public void signIn(View view){
+        String emailTxt = email.getText().toString();
+        String passTxt = pass.getText().toString();
+
+        if (emailTxt.isEmpty() || passTxt.isEmpty()){
+            Toast.makeText(this, "Please fill in the blanks!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            mAuth.signInWithEmailAndPassword(emailTxt, passTxt)
+                    .addOnCompleteListener((task) ->{
+                        if (task.isSuccessful()){
+                            Intent intent = new Intent(this, HomeActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
     }
 }
