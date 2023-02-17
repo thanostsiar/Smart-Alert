3cb440538df2b52b898a89e5250a5f4029145fa5
thanostsiar <thanososfp.tt@gmail.com>
2023-02-17 12:49:47 +0200

    Whenever the user signs in, his name and email are shown in the app drawer

--- SmartAlert/app/src/main/java/com/unipi/tsiaras/smartalert/MainActivity.java
+++ SmartAlert/app/src/main/java/com/unipi/tsiaras/smartalert/MainActivity.java
@@ -8,21 +8,48 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
 
+    TextView name, email;
+
     FirebaseAuth mAuth;
 
     Toolbar toolbar;
+
+    NavigationView navigationView;
+
+    FirebaseDatabase database;
+
+    DatabaseReference usersRef;
+
+    View headerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +57,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+
+        database = FirebaseDatabase.getInstance();
+
+        usersRef = database.getReference("users");
+
+        navigationView = findViewById(R.id.nav_view);
+
+        headerView = navigationView.getHeaderView(0);
+
+        name = headerView.findViewById(R.id.navName);
+
+        email = headerView.findViewById(R.id.navEmail);
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -46,6 +85,74 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_home);
         }
+
+        if (mAuth.getCurrentUser() != null){
+            String userId = mAuth.getUid();
+            usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        String nameDB = snapshot.child(userId).child("name").getValue(String.class);
+                        String emailDB = snapshot.child(userId).child("email").getValue(String.class);
+                        name.append(nameDB);
+                        email.append(emailDB);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+            /*usersRef.child(userId).child("image").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        String imageUrl = snapshot.child()
+                    }
+                    //String imageUrl = snapshot.getValue(String.class);
+                    Picasso.get().load(imageUrl).into(imageView);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });*/
+            /*usersRef.child(userId).child("image").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String imageUrl = snapshot.getValue(String.class);
+                    if (imageUrl != null) {
+                        Picasso.get().load(imageUrl).into(imageView);
+                        //imageView.setImageResource(R.drawable.person);
+                        //storageReference.child(imageUrl).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            /*@Override
+                            public void onSuccess(Uri uri) {
+                                imageView.setImageResource(R.drawable.person);
+                            }
+                        });
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.e("Error", "onCancelled", error.toException());
+                }
+            });*/
+        }
+
+        //storageReference = storage.getReference().child("2131165429");
+
+        /*storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                if (imageView != null) {
+                    //Picasso.get().load(uri).into(imageView);
+                    imageView.setImageResource(R.drawable.person);
+                }
+            }
+        });*/
     }
 
     @Override
