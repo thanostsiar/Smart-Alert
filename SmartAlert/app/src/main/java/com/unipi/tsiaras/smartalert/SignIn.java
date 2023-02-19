42be427584acf3d4734301c062dd01120279d158
jasonbarberis <59922675+jasonbarberis@users.noreply.github.com>
2023-02-19 23:58:40 +0200

    Attempted language integration

--- SmartAlert/app/src/main/java/com/unipi/tsiaras/smartalert/SignIn.java
+++ SmartAlert/app/src/main/java/com/unipi/tsiaras/smartalert/SignIn.java
@@ -1,10 +1,15 @@
 package com.unipi.tsiaras.smartalert;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Config;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +18,9 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class SignIn extends AppCompatActivity {
+import java.util.Locale;
+
+public class SignIn extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     FirebaseAuth mAuth;
     EditText email, pass;
@@ -21,6 +28,10 @@ public class SignIn extends AppCompatActivity {
     Button btn_sign_in;
     FirebaseDatabase database;
     DatabaseReference reference;
+    String[] countryNames={"English","Ελληνικά"};
+    int[] flags = {R.drawable.uk_flag, R.drawable.gre_flag};
+    boolean first_trigger = true;
+    int langpos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,10 @@ public class SignIn extends AppCompatActivity {
         pass = findViewById(R.id.et_password);
         btn_sign_up = findViewById(R.id.button_signup);
         btn_sign_in = findViewById(R.id.button_signin);
+        Spinner spin = (Spinner) findViewById(R.id.spinner_);
+        spin.setOnItemSelectedListener(this);
+        CustomAdapter customAdapter=new CustomAdapter(getApplicationContext(),flags,countryNames);
+        spin.setAdapter(customAdapter);
 
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
@@ -75,4 +90,50 @@ public class SignIn extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+        String selectedlang = adapterView.getItemAtPosition(position).toString();
+        if(first_trigger){
+            first_trigger = false;
+        }else{
+            if(selectedlang.equals("English")){
+                langpos = 0;
+            }
+            else if(selectedlang.equals("Ελληνικά")){
+                langpos = 1;
+            }
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+    public void changelang(View view){
+        switch (langpos){
+            case 0:
+                updateconfig("en");
+                break;
+            case 1:
+                updateconfig("el");
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    @SuppressWarnings("deprecation")
+    public void updateconfig(String s){
+        Locale locale=new Locale(s);
+        Locale.setDefault(locale);
+        Configuration configuration=new Configuration();
+        configuration.setLocale(locale);
+        getBaseContext().getResources().updateConfiguration(configuration,
+                getBaseContext().getResources().getDisplayMetrics());
+        Bundle bundle=new Bundle();
+        onCreate(bundle);
+        setTitle(R.string.app_name);
+    }
 }
